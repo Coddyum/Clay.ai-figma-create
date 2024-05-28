dd390b9f0c32b62269b5125ddac35544f8af9b07
Maxence <coddyumpro@gmail.com>
2024-05-29 00:36:21 +0200

    add link to a navbar + install heroicons/react

--- src/assets/components/common/Navbar/NavBar.jsx
+++ src/assets/components/common/Navbar/NavBar.jsx
@@ -1,10 +1,29 @@
 import React from "react";
-import "./NavBar.css"
+import "./NavBar.css";
+import { ChevronDownIcon } from "@heroicons/react/16/solid";
 
 function NavBar() {
-    return(
-        <div className="w-screen h-16 bg-gray-900">
-            <h1 className="ml-24 my-4 font-bold line-height32 bg-gradient-to-r from-slate-50  via-slate-100 via-50% to-slate-800 inline-block text-transparent bg-clip-text ">clay.ai</h1>
+    return (
+        <div className="w-screen h-16 bg-gray-900 flex items-center">
+            <h1 className="ml-24 my-4 font-bold line-height32 bg-gradient-to-r from-slate-50  via-slate-100 via-50% to-slate-800 inline-block text-transparent bg-clip-text ">
+                clay.ai
+            </h1>
+            <div className="a-ml-507 inline-block text-slate-500">
+                <a className="text-slate-200" href="#">
+                    Home
+                </a>
+                <a className="ml-8 " href="#">
+                    Pages
+                    <ChevronDownIcon className="inline-block w-5 h-5 text-slate-500" />
+                </a>
+                <a className="ml-8 " href="#">
+                    Resources
+                    <ChevronDownIcon className="inline-block w-5 h-5 text-slate-500" />
+                </a>
+                <a className="ml-8" href="#">
+                    Contact
+                </a>
+            </div>
         </div>
     );
 }
