ab48445a0edb2d6a368facfbf979eff65cc983d4
Maxence <coddyumpro@gmail.com>
2024-05-29 23:48:47 +0200

    style button

--- src/assets/components/common/HeroSection/HeroSection.jsx
+++ src/assets/components/common/HeroSection/HeroSection.jsx
@@ -3,18 +3,27 @@ import "./HeroSection.css";
 
 function HeroSection() {
     return (
-        <div className="w-screen h-auto pt-24 px-10 bg-blue-400">
-            <div className="wh-100 bg-red-500 mx-auto"></div>
-            <div className="mx-10 h-auto my-24 flex gap-x-30 bg-green-600">
+        <div className="w-screen h-auto pt-24 px-10 bg-gray-900 overflow-x-hidden">
+            <div className="wh-100 mx-auto"></div>
+            <div className="mx-10 w-1008 h-auto my-6 flex gap-x-32">
                 <div className="w-488 h-auto">
-                    <div className="rounded-3xl p-2 text-sm bg-gradient-to-r from-slate-500  to-slate-900 inline-block">
-                        <h4 className="text-white"><span className=" text-white rounded-xl p-1 bg-gradient-to-t to-boston-blue-600 from-bermuda-300">New</span> Top selling on Framer</h4>
+                    <div className="pl-2 pr-4 py-2 rounded-3xl inline-block border-2 border-gray-700 border-solid bg-gradient-to-r from-gray-600 via-gray-900 to-gray-900">
+                        <h4 className="text-white">
+                            <span className="mr-2 text-white rounded-2xl px-2 bg-gradient-to-t to-boston-blue-600 from-bermuda-300">
+                                New
+                            </span>
+                            Top selling on Framer
+                        </h4>
                     </div>
-                    <h1 className="font-bold text-white text-5-5xl text-left my-6">Boost your Productivity with Creative AIs</h1>
+                    <h1 className="font-bold text-white text-5-5xl text-left my-6">
+                        Boost your Productivity with Creative AIs
+                    </h1>
+                    <p className="text-xl text-gray-500">
+                        Specialising in crafting visually stunning and highly
+                        functional user interfaces.
+                    </p>
                 </div>
-                <div>
-
-                </div>
+                <div></div>
             </div>
         </div>
     );
