87c649bd09e8326393c1dbef508f47e4e88d3b0e
Maxence <coddyumpro@gmail.com>
2024-05-29 18:57:39 +0200

    hero

--- src/assets/Pages/Home/Home.jsx
+++ src/assets/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import NavBar from "../../components/common/Navbar/NavBar";
 import MovingBanner from "../../components/common/TopMovingBanner/MovingBanner";
+import HeroSection from "../../components/common/HeroSection/HeroSection";
 
 function Home() {
     return(
         <>
             <NavBar/>
             <MovingBanner/>
+            <HeroSection/>
         </>
     );
 }
---
+++ src/assets/components/common/HeroSection/HeroSection.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./HeroSection.css";
+
+function HeroSection() {
+    return (
+        <div className="w-screen h-auto pt-24 px-10 bg-blue-400">
+
+        </div>
+    );
+}
+
+export default HeroSection;
